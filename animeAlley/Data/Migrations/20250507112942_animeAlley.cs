7e054b910515ebab6ff08ffcb1dd3ea60c851e34
Daniel Domingues <daniel.domingues718@gmail.com>
2025-05-07 12:31:08 +0100

    Base de dados, concertada

---
+++ animeAlley/Data/Migrations/20250507112942_animeAlley.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace animeAlley.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class animeAlley : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
